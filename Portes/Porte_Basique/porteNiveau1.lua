b9f1596f9bcf7c7e3c14ad4732b70e92c8c21253
Aqua_Keyn <keynmailgg@gmail.com>
2023-03-16 21:51:01 +1100

    MAJ Porte lvl 1
    Ajout d'un script et ajustements

---
+++ Portes/Porte_Basique/porteNiveau1.lua
@@ -0,0 +1,17 @@
+--______________VARIABLES______________--
+
+local porte = script.Parent  -- Initialise la variable de la porte
+local clickDetector = porte.clickDetector -- Initialise la variable du ClickDetector
+
+--______________FONCTION______________--
+
+local function activation() -- Initialisation de la fonction locale "activation"
+    if porte.Transparency == 0 and porte.CanCollide == true then -- Si la porte est visible et si le joueur ne peut pas la traverser, alors
+        porte.Transparency = 1 and porte.CanCollide = false -- Rend la porte invisible et l'on peut la traverser
+    else -- Sinon
+        porte.Transparency = 0 and porte.CanCollide = true -- Rendre la porte visible et la rendre intraversable
+    end
+end
+
+clickDetector.MouseButton1Click:Connect(activation) -- Si le joueur est sur PC, déclencher la fonction au clique gauche
+clickDetector.TouchTap:Connect(activation) -- Si le joueur est sur Mobile, déclencher la fonction à l'appui
