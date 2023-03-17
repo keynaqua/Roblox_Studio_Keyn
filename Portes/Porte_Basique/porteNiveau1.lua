1a2d7ecbfe3b29824ef67b53854c5886c3e0a86e
Aqua_Keyn <keynmailgg@gmail.com>
2023-03-18 00:16:18 +1100

    *edit*
    Edit du README principal et du script de la première porte

--- Portes/Porte_Basique/porteNiveau1.lua
+++ Portes/Porte_Basique/porteNiveau1.lua
@@ -1,17 +1,16 @@
 --______________VARIABLES______________--
 
 local porte = script.Parent  -- Initialise la variable de la porte
-local clickDetector = porte.clickDetector -- Initialise la variable du ClickDetector
+local clickDetector = porte.ClickDetector -- Initialise la variable du ClickDetector
 
 --______________FONCTIONS______________--
 
-local function activation() -- Initialisation de la fonction locale "activation"
-    if porte.Transparency == 0 and porte.CanCollide == true then -- Si la porte est visible et si le joueur ne peut pas la traverser, alors
-        porte.Transparency = 1 and porte.CanCollide = false -- Rend la porte invisible et l'on peut la traverser
-    else -- Sinon
-        porte.Transparency = 0 and porte.CanCollide = true -- Rendre la porte visible et la rendre intraversable
-    end
-end
-
-clickDetector.MouseButton1Click:Connect(activation) -- Si le joueur est sur PC, déclencher la fonction au clique gauche
-clickDetector.TouchTap:Connect(activation) -- Si le joueur est sur Mobile, déclencher la fonction à l'appui
+clickDetector.MouseClick:Connect(function() -- Lorsque que le joueur clique sur la porte, on lance la fonction
+	if porte.Transparency == 0 and porte.CanCollide == true then -- Si la porte est visible et si le joueur ne peut pas la traverser, alors
+		porte.Transparency = 0.8 -- Rend la porte presque invisible
+		porte.CanCollide = false -- On peut traverser la porte
+	else -- Sinon
+		porte.Transparency = 0 -- Rend la porte visible
+		porte.CanCollide = true -- On ne peut plus traverser la porte
+	end
+end)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Roblox_Studio_Keyn
- Je mets mes codes utilisables sur roblox studio
+## Bonjour, je suis Keyn, et je developpe des trucs sur Roblox. Ici, vous pouvez retrouver mes scripts et méthodes pour créer tous types de choses :D
+
+##### Je développe depuis longtemps sur Roblox mais pas très sérieusement, donc mes objets ne sont surement pas parfaits. Malgré tout, j'essaie d'aider via ce GitHub ainsi que le Discord, ce que je n'ai pas eu et qui aurait pû m'aider à apprendre plus vite et mieux.
+
+> Je vais faire des tutoriels sur YouTube pour montrer comment faire les objets donc n'hésites pas à passer !
